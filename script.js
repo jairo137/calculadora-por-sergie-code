55a3e215f5be9176dc97de04b20765a5cea27440
jairo <none>
2024-06-27 20:48:19 +0200

    haciendo una prueba

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function agregar(valor){
 document.getElementById('pantalla').value+=valor;
 }
 
+//Borra los datos de la pantalla
 function borrar () {
     document.getElementById('pantalla').value = '';
 }
-
+//calcula los datos que aparecen en la pantalla
 function calcular () {
     const valorPantalla=document.getElementById('pantalla').value;
     const resultado = eval(valorPantalla);
